f49b604f40c80b72a6fac63a963dd2e0315b332f
Stian Holmås <stian@holmas.com>
2018-01-03 13:32:43 +0100

    SCM test 4

--- src/main/java/org/cytoscape/biogwplugin/internal/query/BGMultiRelationsQuery.kt
+++ src/main/java/org/cytoscape/biogwplugin/internal/query/BGMultiRelationsQuery.kt
@@ -18,29 +18,5 @@ open class BGMultiRelationsQuery(serviceManager: BGServiceManager, override var 
     }
 
     // TODO: Verify that this override isn't needed.
-    /*
-    override fun run() {
-        taskMonitor?.setTitle("Searching for relations...")
-        val uri = encodeUrl()?.toURI()
-        if (uri != null) {
-            val httpGet = HttpGet(uri)
-            val response = client.execute(httpGet)
-            val statusCode = response.statusLine.statusCode
 
-            val data = EntityUtils.toString(response.entity)
-            if (statusCode > 204) {
-                throw Exception("Server connection failed with code "+statusCode+": \n\n"+data)
-            }
-            //print(data)
-            val reader = BufferedReader(StringReader(data))
-            client.close()
-            taskMonitor?.setTitle("Loading results...")
-            parser.parseRelations(reader, returnType, taskMonitor) {
-                returnData = it as? BGReturnData ?: throw Exception("Invalid return data!")
-                taskMonitor?.setTitle("Loading results...")
-                runCompletions()
-            }
-        }
-    }
-    */
 }
